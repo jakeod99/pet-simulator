6c445ac4c6f9dbb1ff911dbe34d2a587f8a32253
Bailey-Pearson <bap3215@rit.edu>
2021-03-01 09:00:11 -0500

    3 equals comparison

--- src/components/GameWindow.js
+++ src/components/GameWindow.js
@@ -6,7 +6,7 @@ import Game from '../assets/game-background.jpg';
 class GameWindow extends Component {
 	render(){
 	    return (
-	    	<div className="game-window" style={{backgroundImage: `url(${this.props.page == 'Adoption' ? Adoption : Game})`, backgroundSize: 'cover'}}>
+	    	<div className="game-window" style={{backgroundImage: `url(${this.props.page === 'Adoption' ? Adoption : Game})`, backgroundSize: 'cover'}}>
 	    		{this.props.children}
 	    	</div>
 	    );
