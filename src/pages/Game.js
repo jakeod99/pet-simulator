7f6c0ad07a6ddd68ac9ed0e20a97d1eca42786cc
Jake O'Donnell <jakeod99@gmail.com>
2021-03-07 18:39:16 -0500

    pets display

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -5,21 +5,15 @@ import GameWindow from '../components/GameWindow.js';
 import Banner from '../components/Banner.js';
 import Actions from '../components/Actions.js';
 import Status from '../components/Status.js';
+import ThoughtBubble from '../components/ThoughtBubble.js';
 import { FaPoop } from "react-icons/fa"; // (F)ont (a)wesome (Poop) - this name cannot change
-import EatIcon from '@material-ui/icons/Restaurant';
-import DrinkIcon from '@material-ui/icons/LocalDrink';
-import BathIcon from '@material-ui/icons/Bathtub';
-import WalkIcon from '@material-ui/icons/DirectionsRun';
-import PlayIcon from '@material-ui/icons/SportsBaseball';
-import TrainIcon from '@material-ui/icons/Sports';
-import HappyIcon from '@material-ui/icons/SentimentSatisfiedAlt';
-import SadIcon from '@material-ui/icons/SentimentDissatisfied';
+import Animations from '../animations.js'
 
 class Game extends Component {
 
     MockPet = { // basic mock pet object until pet logic is ready to swap in
         name: "Blobby",
-        type: "Blob",
+        type: "blob",
         state: "idle",
         poop: true,
         thought: "happy",
@@ -27,81 +21,29 @@ class Game extends Component {
         hunger: 25
     }
 
-    thoughtBubble() { // thoughts here will likely change, these are just Jake's placeholders
-        switch (this.MockPet.thought) {
-            case "eat":
-                return (
-                    <div class="thought-bubble">
-                        <EatIcon className="thought" fontSize="large" /> 
-                    </div>
-                );
-            case "drink":
-                return (
-                    <div class="thought-bubble">
-                        <DrinkIcon className="thought" fontSize="large" /> 
-                    </div>
-                );
-            case "bath":
-                return (
-                    <div class="thought-bubble">
-                        <BathIcon className="thought" fontSize="large" /> 
-                    </div>
-                );
-            case "walk":
-                return (
-                    <div class="thought-bubble">
-                        <WalkIcon className="thought" fontSize="large" /> 
-                    </div>
-                );
-            case "play":
-                return (
-                    <div class="thought-bubble">
-                        <PlayIcon className="thought" fontSize="large" /> 
-                    </div>
-                );
-            case "train":
-                return (
-                    <div class="thought-bubble">
-                        <TrainIcon className="thought" fontSize="large" /> 
-                    </div>
-                );
-            case "happy":
-                return (
-                    <div class="thought-bubble">
-                        <HappyIcon className="thought" fontSize="large" /> 
-                    </div>
-                );
-            case "sad":
-                return (
-                    <div class="thought-bubble">
-                        <SadIcon className="thought" fontSize="large" /> 
-                    </div>
-                );
-            default:
-                return (
-                    <div class="thought-bubble"></div>
-                );
-        }
-    }
-
     render() {
+        let displayName = this.MockPet.name + " the " + this.MockPet.type[0].toUpperCase() + this.MockPet.type.substr(1);
+
         return (
             <div className="App">
                 <div className="Page">
                     <PageTitle />
                     <GameWindow page={'Game'}>
-                        <Banner bannerMessage={this.MockPet.name + " the " + this.MockPet.type}/>
+                        <Banner bannerMessage={displayName}/>
                         <div class="game-box">
-                            <Actions />
-                            <div class={"poop-container " + (this.MockPet.poop ? "" : "hide")}>
-                                <FaPoop size="3em" style={{ color: 'var(--brown)' }} /> 
-                            </div>
-                            <div class="pet-container">
-                                <div class={"thought-bubble-container " + (this.MockPet.thought === "none" ? "hide" : "")}>
-                                    { this.thoughtBubble() }
+                            <div class="left-game">
+                                <Actions />
+                                <div class={"poop-container " + (this.MockPet.poop ? "" : "hide")}>
+                                    <FaPoop size="3em" style={{ color: 'var(--brown)' }} /> 
                                 </div>
                             </div>
-                            <Status health={this.MockPet.health} hunger={this.MockPet.hunger} />
+                            <div class="center-game">
+                                <ThoughtBubble thought={this.MockPet.thought} />
+                                <img src={Animations[this.MockPet.type][this.MockPet.state]} class="pet" />
+                            </div>
+                            <div class="right-game">
+                                <Status health={this.MockPet.health} hunger={this.MockPet.hunger} />
+                            </div>
                         </div>
                     </GameWindow>
                 </div>
